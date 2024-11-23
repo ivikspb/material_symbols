407f08542c65b4fbcde283ccbe4480bcd271810b
Viktor Ivanov <ivikspb@gmail.com>
2024-11-24 01:18:55 +0300

    Material Symbols

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Material Symbols",
+    "summary": "Material Symbols",
+    "description": """Material Design Icons from Google""",
+    "author": "ivik",
+    "category": "Web",
+    "version": "18.0.0.1",
+    "license": "LGPL-3",
+    "depends": [
+        "web",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "material_symbols/static/src/css/material-symbols.css",
+        ],
+        "web.assets_frontend": [
+            "material_symbols/static/src/css/material-symbols.css",
+        ],
+        "web.report_assets_common": [
+            "material_symbols/static/src/css/material-symbols.css",
+        ],
+    },
+    "application": True,
+}
